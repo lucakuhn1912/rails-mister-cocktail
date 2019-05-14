f8616ca0c45a5dcd1e4357ae5bcc2bca7772672b
Luca Kuhn <luca.kuhn@hotmail.de>
2019-05-14 16:56:49 +0100

    cocktails

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+
+
+
+response = JSON.parse(open('https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list').read)
+
+response["drinks"].each do |x|
+  Ingredient.create(name: x["strIngredient1"])
+end
+
+
+
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+
+Cocktail.create(name: "Mojito")
+Cocktail.create(name: "Lasting Passion")
+Cocktail.create(name: "Martini")
+Cocktail.create(name: "Rum Punch")
